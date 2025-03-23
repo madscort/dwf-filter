3981a092a43d148ecf17e7e8c3cc74fd9709bf29
Mads Nielsen <madsn@msn.com>
2025-03-23 10:38:42 +0100

    restructure training

--- src/03_train_model.py
+++ src/03_train_model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from copy import deepcopy
-import sys
 from pathlib import Path
 from sklearn.metrics import f1_score
 from sklearn.model_selection import KFold
@@ -9,17 +8,20 @@ from models.model import ML_model
 from utils import feature_subsets_snv, feature_subsets_indel, feature_subsets_all, extract_features
 import argparse
 import logging
+import yaml
+
+def load_config(config_path="config/model_config.yaml"):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
 def inner_cross_validation(X_train, y_train, model, inner_folds=5, random_seed=42):
-    """
-    Perform inner cross-validation for model selection.
-    """
+    """Perform inner cross-validation for model selection."""
     inner_cv = KFold(n_splits=inner_folds, shuffle=True, random_state=random_seed)
     
     best_model = None
     best_parameter = None
     best_score = -np.inf
-    best_threshold = None
 
     # Inner loop for model selection
     for inner_train_index, inner_val_index in inner_cv.split(X_train):
@@ -53,14 +55,11 @@ def inner_cross_validation(X_train, y_train, model, inner_folds=5, random_seed=4
                 best_model = deepcopy(current_model)
             best_parameter = current_parameter
             best_score = score
-            best_threshold = model.optimal_threshold
     
     return best_model, best_parameter, best_score
 
 def outer_cross_validation(X, y, model, outer_folds=5, inner_folds=5, random_state=42, info=None, repetition=1):
-    """
-    Perform outer cross-validation to evaluate the best model.
-    """
+    """ Perform outer cross-validation to evaluate the best model."""
     outer_cv = KFold(n_splits=outer_folds, shuffle=True, random_state=random_state)
     outer_results = []
     prediction_data = []
@@ -116,42 +115,19 @@ def outer_cross_validation(X, y, model, outer_folds=5, inner_folds=5, random_sta
     return outer_results, prediction_df
 
 
-def main(model="gmm", vtype="snv", log_path="output/test.log", dataset_path=None, output_path="output/testpreds.tsv"):
-    """
-    Main function to handle model training and evaluation.
+def main(model="gmm", vtype="snv", log_path="output/test.log", dataset_path=None, output_path="output/testpreds.tsv", config_path="config/model_config.yaml"):
 
-    Arguments:
-    - model: str, model type (default: "gmm")
-    - vtype: str, variant type ('snv', 'indel', 'all')
-    - log_path: str, path to the log file
-    - dataset_path: str, path to the dataset file
-    - output_path: str, path to save the output predictions file
-    """
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+    config = load_config(config_path)
+    
+    default_dataset_paths = config.get('dataset_paths', {})
 
-    # Define fixed dataset paths for each variant type
-    default_dataset_paths = {
-        "snv": "/Users/madsnielsen/Predisposed/data/datasets/E1LLC/processed_dataset_snv.tsv",
-        "indel": "/Users/madsnielsen/Predisposed/data/datasets/E1LLC/processed_dataset_indel.tsv",
-        "all": "/Users/madsnielsen/Predisposed/data/datasets/E1LLC/processed_dataset.tsv",
-    }
-
-    default_dataset_paths = {
-        "snv": "/Users/madsnielsen/Predisposed/projects/filter_manuscript_repo/input/dataset_snv.tsv",
-        "indel": "/Users/madsnielsen/Predisposed/projects/filter_manuscript_repo/input/dataset_indel.tsv",
-        "all": "/Users/madsnielsen/Predisposed/projects/filter_manuscript_repo/input/dataset.tsv",
-    }
-
-    # If no dataset path is provided, use the default for the given variant type
     if dataset_path is None:
         if vtype in default_dataset_paths:
             dataset_path = default_dataset_paths[vtype]
         else:
             raise ValueError(f"Invalid variant type: {vtype}")
 
-    # Set up logging
-    logging.basicConfig(filename=log_path, level=logging.INFO)
-
-    # Select feature subsets based on `vtype`
     if vtype == 'snv':
         feature_subsets = feature_subsets_snv
     elif vtype == 'indel':
@@ -161,88 +137,47 @@ def main(model="gmm", vtype="snv", log_path="output/test.log", dataset_path=None
     else:
         raise ValueError(f"Invalid variant type: {vtype}")
     
-    X, y, info, feature_subsets_np = extract_features(dataset_path, feature_subsets)    
-    outer_folds=10
-    inner_folds=5
-    repetitions=5
-    random_seed=2
+    X, y, info, feature_subsets_np = extract_features(dataset_path, feature_subsets)
 
-    LN_params = {
-        'class_weight': ['balanced'],
-        'feature_subset': ['all'],
-        'C': [0.1, 1, 1.3, 1.5, 2],
-        'max_iter': [10000],
-        'penalty': ['l1', 'elasticnet', 'l2']
-    }
-    LN_model = ML_model('logistic_regression', LN_params, available_subsets=feature_subsets_np, random_state=random_seed)
+    cv_settings = config.get('cv_settings', {})
+    outer_folds = cv_settings.get('outer_folds', 10)
+    inner_folds = cv_settings.get('inner_folds', 5)
+    repetitions = cv_settings.get('repetitions', 5)
+    random_seed = cv_settings.get('random_seed', 2)
 
-    xgb_params = {
-        'class_weight': ['balanced'],
-        'feature_subset': ['all'],
-        'n_estimators': [200, 250],
-        'max_depth': [8,9],
-        'learning_rate': [0.2, 0.3],
-        'colsample_bytree': [0.8]
-    }
-    XGB_model = ML_model('xgboost', xgb_params, random_state=random_seed, available_subsets=feature_subsets_np)
+    model_mapping = {}
+
+    if 'logistic_regression' in config:
+        LN_params = config['logistic_regression']
+        model_mapping['logistic_regression'] = ML_model('logistic_regression', LN_params, available_subsets=feature_subsets_np, random_state=random_seed)
     
-    lgbm_params = {
-        'feature_subset': ['all'],
-        'class_weight': ['balanced'],
-        'n_estimators': [12, 25, 50, 100],
-        'max_depth': [3, 5, 7, 10],
-        'learning_rate': [0.2, 0.5, 0.9],
-        'colsample_bytree': [0.8],
-    }
+    if 'xgboost' in config:
+        xgb_params = config['xgboost']
+        model_mapping['xgboost'] = ML_model('xgboost', xgb_params, random_state=random_seed, available_subsets=feature_subsets_np)
 
-    lgbm_params = {}
+    if 'gmm' in config:
+        gmm_params = config['gmm']
+        model_mapping['gmm'] = ML_model('gmm', gmm_params, transformation='standard', random_state=random_seed, available_subsets=feature_subsets_np)
 
+    if 'random_forest' in config:
+        rf_params = config['random_forest']
+        model_mapping['random_forest'] = ML_model('random_forest', rf_params, random_state=random_seed, available_subsets=feature_subsets_np)
 
-    LGBM_model = ML_model('lightgbm', lgbm_params, random_state=random_seed, available_subsets=feature_subsets_np)
-
-    gmm_params = {
-        'feature_subset': ['normal_loose'],
-        'class_weight': ['balanced'],
-        'n_components': [1,2,4,6,8,10,12],
-        'covariance_type': ['full'],
-    }
-    GM_model = ML_model('gmm', gmm_params, transformation='standard', random_state=random_seed, available_subsets=feature_subsets_np)
-
-
-    rf_params = {
-        'feature_subset': ['all'],
-        'class_weight': ['balanced'],
-        'n_estimators': [150,200],
-        'max_depth': [12, 14, 16],
-        'min_samples_leaf': [8],
-    }
-    RF_model = ML_model('random_forest', rf_params, random_state=random_seed, available_subsets=feature_subsets_np)
-
-    model_mapping = {
-            'logistic_regression': LN_model,
-            'xgboost': XGB_model,
-            'lightgbm': LGBM_model,
-            'random_forest': RF_model,
-            'gmm': GM_model
-    }
-
-    selected_model = model_mapping[model]
+    selected_model = model_mapping.get(model)
+    if selected_model is None:
+        raise ValueError(f"Invalid model selection: {model}")
     models = [selected_model]
     all_predictions = []
     for model in models:
         logging.info(f"Running model training and evaluation for model: {model.model_type}")
-        print(f"Running model training and evaluation for model: {model.model_type}")
         for i in range(repetitions):
-            print(f"Running repetition {i + 1}/{repetitions}...")
             logging.info(f"Running repetition {i + 1}/{repetitions}...")
             
             random_state_repetition = random_seed + i 
             results, prediction_df = outer_cross_validation(X, y, model, outer_folds, inner_folds, random_state_repetition, info, repetition=i+1)
-            
-            print(f"Results for repetition {i + 1}:")
+
             logging.info(f"Results for repetition {i + 1}:")
             for j, result in enumerate(results):
-                print(f"Fold {j + 1}: Inner Score: {result['best_inner_score']:.4f}, Outer Score: {result['outer_test_score']:.4f}, Best Parameter: {result['best_parameter']}")
                 logging.info(f"Fold {j + 1}: Inner Score: {result['best_inner_score']:.4f}, Outer Score: {result['outer_test_score']:.4f}, Best Parameter: {result['best_parameter']}")
             all_predictions.append(prediction_df)
     
@@ -251,12 +186,13 @@ def main(model="gmm", vtype="snv", log_path="output/test.log", dataset_path=None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run model training and evaluation.')
-    parser.add_argument('--model', type=str, choices=['logistic_regression', 'xgboost','lightgbm', 'random_forest', 'gmm'],
-                        help='Model type to use for training: logistic_regression, xgboost, lightgbm, random_forest, gmm', default='gmm')
+    parser.add_argument('--model', type=str, choices=['logistic_regression', 'xgboost', 'random_forest', 'gmm'],
+                        help='Model type to use for training: logistic_regression, xgboost, random_forest, gmm', default='gmm')
     parser.add_argument('--vtype', type=str, help='Variant type to use for training: snv, indel, all', default='snv')
     parser.add_argument('--log', type=str, help='Path to the log file', default='output/test.log')
     parser.add_argument('--data', type=str, help='Path to the dataset file', default=None)
     parser.add_argument('--output', type=str, help='Path to save the output predictions file', default='output/testpreds.tsv')
+    parser.add_argument('--config', type=str, help='Path to the YAML configuration file', default='config/model_config.yaml')
 
     args = parser.parse_args()
 
@@ -264,13 +200,22 @@ if __name__ == "__main__":
         model='gmm',
         vtype='indel',
         log_path='test.log',
+        config_path='cv_config.yaml',
         output_path='/Users/madsnielsen/Predisposed/projects/filter_manuscript_repo/output/test_preds/training_predictions_gmm_indel.tsv'
     )
 
+#     main(
+#         model=args.model,
+#         vtype=args.vtype,
+#         log_path=args.log,
+#         dataset_path=args.data,
+#         output_path=args.output,
+#         config_path=args.config
+#     )
 # if __name__ == "__main__":
 #     parser = argparse.ArgumentParser(description='Run model training and evaluation.')
-#     parser.add_argument('--model', type=str, choices=['logistic_regression', 'xgboost', 'random_forest', 'gmm'],
-#                         help='Model type to use for training: logistic_regression, xgboost, random_forest, gmm', default='gmm')
+#     parser.add_argument('--model', type=str, choices=['logistic_regression', 'xgboost','lightgbm', 'random_forest', 'gmm'],
+#                         help='Model type to use for training: logistic_regression, xgboost, lightgbm, random_forest, gmm', default='gmm')
 #     parser.add_argument('--vtype', type=str, help='Variant type to use for training: snv, indel, all', default='snv')
 #     parser.add_argument('--log', type=str, help='Path to the log file', default='output/test.log')
 #     parser.add_argument('--data', type=str, help='Path to the dataset file', default=None)
@@ -279,9 +224,8 @@ if __name__ == "__main__":
 #     args = parser.parse_args()
 
 #     main(
-#         model=args.model,
-#         vtype=args.vtype,
-#         log_path=args.log,
-#         dataset_path=args.data,
-#         output_path=args.output
+#         model='gmm',
+#         vtype='indel',
+#         log_path='test.log',
+#         output_path='/Users/madsnielsen/Predisposed/projects/filter_manuscript_repo/output/test_preds/training_predictions_gmm_indel.tsv'
 #     )
